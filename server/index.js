b27be21b2040e2324d6a4d7ade6217546fc9502a
sanjana <sanjanabadam@gmail.com>
2017-10-18 15:33:48 -0700

    reading the PORT value from env variable

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
 	res.send({Hi: 'There'});
 });
 
-app.listen(5000);
-
+const PORT = process.env.PORT || 5000; //listen to the port given as the env variable or default to 5000
+app.listen(PORT);
