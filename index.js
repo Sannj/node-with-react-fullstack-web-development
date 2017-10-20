e305f490ca893f9e04a78720f27c7f600c1eb1eb
sanjana <sanjanabadam@gmail.com>
2017-10-20 08:58:33 -0700

    progress till Section 3 Lec 19

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express'); //using common js modules on server side. 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({Hello: 'There'});
-});
+passport.use(new GoogleStrategy()); //assigning a new Google Strategy to passport. 
 
 const PORT = process.env.PORT || 5000; //listen to the port given as the env variable or default to 5000
 app.listen(PORT);
